0c5496385c978541934c91927e63bd334439dae2
ldrc2895 <ldrc2895@gmail.com>
2017-08-18 12:10:45 -0600

    Primer Version

---
+++ backend/models/Competencias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LuisDiego
+ * Date: 18/08/2017
+ * Time: 11:06
+ */
+
+namespace app\models;
+
+
+class Competencias extends \yii\db\ActiveRecord
+{
+
+
+
+}
