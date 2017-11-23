29471d6a41b6dff3b171508e3ca5b171125284ff
ldrc2895 <ldrc2895@gmail.com>
2017-11-23 11:45:32 -0600

    Ejemplo conexion Oracle

---
+++ common/config/conf-oracle.php
@@ -0,0 +1,26 @@
+<?php
+
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'oci:dbname=//ip:puerto/nombre_servicio;charset=UTF8',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,
+    // Duration of schema cache.
+    'schemaCacheDuration' => 20000,
+    'schemaMap'=>['oci'=>'common\components\ESchemaOci'],// Si es base de datos Oracle habilitar este atributo
+    'attributes' => [
+        PDO::ATTR_CASE => PDO::CASE_LOWER,
+    ],
+    // Si es base de datos Oracle activar este comando, cambia la fecha de la sesion
+    'on afterOpen'=>function ($event)
+    {
+        $event->sender->createCommand("ALTER SESSION SET NLS_DATE_FORMAT ='yyyy-mm-dd hh24:mi:ss'")->execute();
+    }
+    /**
+     * Si es base de datos Oracle, descomentar las lineas que corresponden con SchemaMap,Attributes y on afterOpen
+     */
+
+];
