b9a1e60995071ae1ba0c628b9d023255f6c6f205
ldrc2895 <ldrc2895@gmail.com>
2017-11-27 11:31:36 -0600

    Rest de Usuarios

--- backend/config/main.php
+++ backend/config/main.php
@@ -77,7 +77,8 @@ return [
                    'class'=>'yii\rest\UrlRule',
                    'controller' => ['movimientos-vacaciones','vacaciones','flujo-proceso-agente-updater',
                                     'enlaces-externos','flujo-proceso-email-sender','empleados','flujos-procesos',
-                                    'relaciones-empleados','puestos','horarios','eventos-calendario-empleado'],
+                                    'relaciones-empleados','puestos','horarios','eventos-calendario-empleado',
+                                    'security-users'],
                    'tokens' => [
                        '{id}'=>'<id:(\\w|-|_)+(,(\\w|-|_)+)*>'
                    ],
---
+++ backend/controllers/SecurityUsersController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LuisDiego
+ * Date: 27/11/2017
+ * Time: 11:20
+ */
+
+namespace backend\controllers;
+
+
+use yii\rest\ActiveController;
+
+class SecurityUsersController extends ActiveController
+{
+    public $modelClass =  'backend\models\SecurityUser';
+
+}
