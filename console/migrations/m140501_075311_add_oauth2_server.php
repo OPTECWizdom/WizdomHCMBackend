19111409779f0d98b44611df3ce0ebf372a7f967
ldrc2895 <ldrc2895@gmail.com>
2018-01-18 12:42:47 -0600

    Actualizacion composer.json y migracion para OAuth

--- console/migrations/m140501_075311_add_oauth2_server.php
+++ console/migrations/m140501_075311_add_oauth2_server.php
@@ -1,19 +1,35 @@
 <?php
-
 use yii\db\Schema;
-
-class m170610_162817_oauth extends \yii\db\Migration
+class m140501_075311_add_oauth2_server extends \yii\db\Migration
 {
-
+    private $driverKeyWords = [
+      "CURRENT_TIMESTAMP"=>[
+            'oci'=>'CURRENT_TIMESTAMP',
+            'mysql'=>'CURRENT_TIMESTAMP',
+            'default'=>'now'
+          ]
+    ];
+    public function getCurrentTimestampKeyWord()
+    {
+        $driverKeywords = $this->driverKeyWords["CURRENT_TIMESTAMP"];
+        $driver = $this->db->driverName;
+        if(key_exists($driver,$driverKeywords))
+        {
+           return $driverKeywords[$driver];
+        }
+        return $driverKeywords['default'];
+    }
     public function mysql($yes,$no='') {
         return $this->db->driverName === 'mysql' ? $yes : $no;
     }
-
-    public function primaryKey($columns = null) {
+    public function isOracle($yes,$no='')
+    {
+        return $this->db->driverName =='oci'? $yes:$no;
+    }
+    public function primaryKeyDefinition($columns) {
         return 'PRIMARY KEY (' . $this->db->getQueryBuilder()->buildColumns($columns) . ')';
     }
-
-    public function foreignKey($columns,$refTable,$refColumns,$onDelete = null,$onUpdate = null) {
+    public function foreignKeyDefinition($columns,$refTable,$refColumns,$onDelete = null,$onUpdate = null) {
         $builder = $this->db->getQueryBuilder();
         $sql = ' FOREIGN KEY (' . $builder->buildColumns($columns) . ')'
             . ' REFERENCES ' . $this->db->quoteTableName($refTable)
@@ -22,19 +38,18 @@ class m170610_162817_oauth extends \yii\db\Migration
             $sql .= ' ON DELETE ' . $onDelete;
         }
         if ($onUpdate !== null) {
-            $sql .= ' ON UPDATE ' . $onUpdate;
+            $sql .= $this->isOracle('',' ON UPDATE ' . $onUpdate);
         }
         return $sql;
     }
-
     public function up()
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
         }
-
-
+        $now            = $this->getCurrentTimestampKeyWord();
+        $on_update_now  = $this->mysql("ON UPDATE $now");
         $transaction = $this->db->beginTransaction();
         try {
             $this->createTable('{{%oauth_clients}}', [
@@ -44,83 +59,71 @@ class m170610_162817_oauth extends \yii\db\Migration
                 'grant_types' => Schema::TYPE_STRING . '(100) NOT NULL',
                 'scope' => Schema::TYPE_STRING . '(2000) DEFAULT NULL',
                 'user_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
-                $this->primaryKey('client_id'),
+                $this->primaryKeyDefinition('client_id'),
             ], $tableOptions);
-
             $this->createTable('{{%oauth_access_tokens}}', [
                 'access_token' => Schema::TYPE_STRING . '(40) NOT NULL',
                 'client_id' => Schema::TYPE_STRING . '(32) NOT NULL',
                 'user_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
-                'expires' => Schema::TYPE_TIMESTAMP . "  DEFAULT CURRENT_TIMESTAMP NOT NULL",
+                'expires' => Schema::TYPE_TIMESTAMP . " DEFAULT $now $on_update_now NOT NULL ",
                 'scope' => Schema::TYPE_STRING . '(2000) DEFAULT NULL',
-                $this->primaryKey('access_token'),
-                $this->foreignKey('client_id','{{%oauth_clients}}','client_id','CASCADE'),
+                $this->primaryKeyDefinition('access_token'),
+                $this->foreignKeyDefinition('client_id','{{%oauth_clients}}','client_id','CASCADE','CASCADE'),
             ], $tableOptions);
-
             $this->createTable('{{%oauth_refresh_tokens}}', [
                 'refresh_token' => Schema::TYPE_STRING . '(40) NOT NULL',
                 'client_id' => Schema::TYPE_STRING . '(32) NOT NULL',
                 'user_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
-                'expires' => Schema::TYPE_TIMESTAMP . "  DEFAULT CURRENT_TIMESTAMP  NOT NULL",
+                'expires' => Schema::TYPE_TIMESTAMP . " DEFAULT $now $on_update_now NOT NULL ",
                 'scope' => Schema::TYPE_STRING . '(2000) DEFAULT NULL',
-                $this->primaryKey('refresh_token'),
-                $this->foreignKey('client_id','{{%oauth_clients}}','client_id','CASCADE'),
+                $this->primaryKeyDefinition('refresh_token'),
+                $this->foreignKeyDefinition('client_id','{{%oauth_clients}}','client_id','CASCADE','CASCADE'),
             ], $tableOptions);
-
             $this->createTable('{{%oauth_authorization_codes}}', [
                 'authorization_code' => Schema::TYPE_STRING . '(40) NOT NULL',
                 'client_id' => Schema::TYPE_STRING . '(32) NOT NULL',
                 'user_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
                 'redirect_uri' => Schema::TYPE_STRING . '(1000) NOT NULL',
-                'expires' => Schema::TYPE_TIMESTAMP . "  DEFAULT CURRENT_TIMESTAMP NOT NULL",
+                'expires' => Schema::TYPE_TIMESTAMP . " DEFAULT $now $on_update_now NOT NULL ",
                 'scope' => Schema::TYPE_STRING . '(2000) DEFAULT NULL',
-                $this->primaryKey('authorization_code'),
-                $this->foreignKey('client_id','{{%oauth_clients}}','client_id','CASCADE'),
+                $this->primaryKeyDefinition('authorization_code'),
+                $this->foreignKeyDefinition('client_id','{{%oauth_clients}}','client_id','CASCADE','CASCADE'),
             ], $tableOptions);
-
             $this->createTable('{{%oauth_scopes}}', [
                 'scope' => Schema::TYPE_STRING . '(2000) NOT NULL',
                 'is_default' => Schema::TYPE_BOOLEAN . ' NOT NULL',
             ], $tableOptions);
-
             $this->createTable('{{%oauth_jwt}}', [
                 'client_id' => Schema::TYPE_STRING . '(32) NOT NULL',
                 'subject' => Schema::TYPE_STRING . '(80) DEFAULT NULL',
                 'public_key' => Schema::TYPE_STRING . '(2000) DEFAULT NULL',
-                $this->primaryKey('client_id'),
+                $this->primaryKeyDefinition('client_id'),
             ], $tableOptions);
-
             $this->createTable('{{%oauth_users}}', [
                 'username' => Schema::TYPE_STRING . '(255) NOT NULL',
                 'password' => Schema::TYPE_STRING . '(2000) DEFAULT NULL',
                 'first_name' => Schema::TYPE_STRING . '(255) DEFAULT NULL',
                 'last_name' => Schema::TYPE_STRING . '(255) DEFAULT NULL',
-                $this->primaryKey('username'),
+                $this->primaryKeyDefinition('username'),
             ], $tableOptions);
-
             $this->createTable('{{%oauth_public_keys}}', [
                 'client_id' => Schema::TYPE_STRING . '(255) NOT NULL',
                 'public_key' => Schema::TYPE_STRING . '(2000) DEFAULT NULL',
                 'private_key' => Schema::TYPE_STRING . '(2000) DEFAULT NULL',
                 'encryption_algorithm' => Schema::TYPE_STRING . '(100) DEFAULT \'RS256\'',
             ], $tableOptions);
-
             // insert client data
             $this->batchInsert('{{%oauth_clients}}', ['client_id', 'client_secret', 'redirect_uri', 'grant_types'], [
                 ['testclient', 'testpass', 'http://fake/', 'client_credentials authorization_code password implicit'],
             ]);
-
             $transaction->commit();
         } catch (Exception $e) {
             echo 'Exception: ' . $e->getMessage() . '\n';
             $transaction->rollback();
-
             return false;
         }
-
         return true;
     }
-
     public function down()
     {
         $transaction = $this->db->beginTransaction();
@@ -133,7 +136,6 @@ class m170610_162817_oauth extends \yii\db\Migration
             $this->dropTable('{{%oauth_access_tokens}}');
             $this->dropTable('{{%oauth_public_keys}}');
             $this->dropTable('{{%oauth_clients}}');
-
             $transaction->commit();
         } catch (Exception $e) {
             $transaction->rollback();
@@ -141,10 +143,8 @@ class m170610_162817_oauth extends \yii\db\Migration
             echo "\n";
             echo get_called_class() . ' cannot be reverted.';
             echo "\n";
-
             return false;
         }
-
         return true;
     }
 }
