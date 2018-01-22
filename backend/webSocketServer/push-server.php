947c1c5be7b75f0feb2c138f6c0c4f18fdc42f98
ldrc2895 <ldrc2895@gmail.com>
2018-01-22 11:17:54 -0600

    Inicio Implementacion Socket Server

---
+++ backend/webSocketServer/push-server.php
@@ -0,0 +1,52 @@
+<?php
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../common/config/bootstrap.php');
+require(__DIR__ . '/../config/bootstrap.php');
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/main.php'),
+    require(__DIR__ . '/../../common/config/main-local.php'),
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main-local.php')
+);
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use backend\webSocketServer\Chat;
+
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    8080
+);
+
+$server->run();
+/*
+$loop   = React\EventLoop\Factory::create();
+$pusher = new \backend\webSocketServer\Chat;
+// Listen for the web server to make a ZeroMQ push after an ajax request
+$context = new React\ZMQ\Context($loop);
+$pull = $context->getSocket(ZMQ::SOCKET_PULL);
+$pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only client that can connect is itself
+$pull->on('message', array($pusher, 'onBlogEntry'));
+*/
+/*
+// Set up our WebSocket server for clients wanting real-time updates
+$webSock = new React\Socket\Server('0.0.0.0:8080', $loop); // Binding to 0.0.0.0 means remotes can connect
+$webServer = new Ratchet\Server\IoServer(
+    new Ratchet\Http\HttpServer(
+        new Ratchet\WebSocket\WsServer(
+            new Ratchet\Wamp\WampServer(
+                $pusher
+            )
+        )
+    ),
+    $webSock
+);
+
+$loop->run();*/
