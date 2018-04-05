4bf2e87830e06f2be7c4b5b1e4c79b7459adaca0
ldrc2895 <ldrc2895@gmail.com>
2018-04-05 15:24:15 -0600

    Proceso

---
+++ common/mail/notificacionConvalidacionConocimiento-html.php
@@ -0,0 +1,68 @@
+<?php
+use yii\helpers\Html;
+use backend\models\proceso\Proceso;
+use backend\models\procesoModelConnector\procesoConvalidacionConocimiento\ProcesoConvalidacionConocimiento;
+use backend\models\convalidacionConocimiento\ConvalidacionConocimiento;
+use backend\models\empleado\Empleado;
+
+/**
+ * @var Proceso $proceso
+ */
+$proceso;
+
+$procesoPK = $proceso->getAttributes(Proceso::primaryKey());
+/**
+ * @var ProcesoConvalidacionConocimiento $procesoConvalidacionConocimiento
+ */
+
+$procesoConvalidacionConocimiento = ProcesoConvalidacionConocimiento::find()->where($procesoPK)->one();
+
+/**
+ * @var ConvalidacionConocimiento $convalidacionConocimiento
+ */
+$convalidacionConocimiento = $procesoConvalidacionConocimiento->getConvalidacionConocimiento()->one();
+
+
+/**
+ * @var Empleado $empleado
+ */
+
+$empleado = $convalidacionConocimiento->getEmpleado()->one();
+
+$nombreEmpleado = mb_convert_case(mb_strtolower($empleado->nombre.' '.$empleado->primer_apellido.' '.$empleado->segundo_apellido),MB_CASE_TITLE);
+$descripcionConocimiento = $convalidacionConocimiento->getAttribute('concimiento_desc');
+
+?>
+
+<div class="site-index">
+
+    <div class="jumbotron">
+        <p>Tiene una nueva solicitud de convalidaciión de conocimiento.</p>
+        <h3>Datos de la convalidación</h3>
+        <table>
+            <tr>
+                <th align = "left" width = "50%">
+                    Nombre del Empleado
+                </th >
+                <th align = "left" width = "50%">
+                    Descripción
+                </th>
+
+            </tr>
+            <tr>
+                <td>
+
+                    <?= Html::encode(($nombreEmpleado));?>
+                </td>
+                <td>
+                    <?= Html::encode($descripcionConocimiento);?>
+                </td>
+                <td>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+
+
+</div>
